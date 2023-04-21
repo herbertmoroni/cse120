145e36850ec7bc1a2a0a7c0fa04c9b102eccc9bd
herbertmoroni <herbertmoroni@hotmail.com>
2023-04-21 16:50:05 -0600

    assigment w02 prove

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,46 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random random = new Random();
+            int magicNumber = random.Next(1, 101);
+            int guess;
+            int numGuesses = 0;
+            string playAgain;
+
+            do
+            {
+                Console.WriteLine("Guess the magic number between 1 and 100.");
+                Console.Write("Enter your guess: ");
+                guess = int.Parse(Console.ReadLine());
+                numGuesses++;
+
+                if (guess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (guess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                    Console.WriteLine("It took you " + numGuesses + " guesses.");
+                    Console.WriteLine();
+                    Console.Write("Do you want to play again? (yes or no): ");
+                    playAgain = Console.ReadLine().ToLower();
+                    if (playAgain != "yes")
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        magicNumber = random.Next(1, 101);
+                        numGuesses = 0;
+                        Console.WriteLine();
+                    }
+                }
+
+            } while (true);
     }
 }
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,46 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        int input;
+
+        do
+        {
+            Console.Write("Enter a list of numbers, type 0 when finished: ");
+            input = int.Parse(Console.ReadLine());
+            
+            if (input != 0)
+            {
+                numbers.Add(input);
+            }
+
+        } while (input != 0);
+
+        int sum = numbers.Sum();
+        double average = numbers.Average();
+        int max = numbers.Max();
+
+        Console.WriteLine("The sum is: " + sum);
+        Console.WriteLine("The average is: " + average);
+        Console.WriteLine("The largest number is: " + max);
+
+        List<int> positives = numbers.Where(n => n > 0).ToList();
+        int minPositive = positives.Min();
+
+        Console.WriteLine("The smallest positive number is: " + minPositive);
+
+        List<int> sorted = numbers.OrderBy(n => n).ToList();
+
+        Console.WriteLine("The sorted list is:");
+        
+        foreach (int num in sorted)
+        {
+            Console.WriteLine(num);
+        }
     }
 }
